c3253f5dbff962bcb88b48d422bcb59b6c0e1279
juan carlos viera <juancarlos.viera@gmail.com>
2026-07-29 11:09:58 -0600

    Actualiza archivo principal con puerto de railway

--- src/index.ts
+++ src/index.ts
@@ -174,4 +174,3 @@ const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Senda corriendo en el puerto ${PORT}`);
 });
-// Actualización forzada para puerto Railway 2
